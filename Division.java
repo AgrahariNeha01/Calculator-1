cf0fb32374afd76c8baf74dabee0582abc92802e
Neha Agrahari <neha16agrahari@gmail.com>
2025-01-17 14:57:04 +0530

    updated division

--- Division.java
+++ Division.java
@@ -1,3 +1,24 @@
+import java.util.*;
 public class Division {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a= sc.nextInt();
+        int b= sc.nextInt();
+        if(a>b)
+        {
+            if(b!=0)
+            System.out.println(a/b);
+            else
+            System.out.println("cant divide");
+        }
+        else {
+            if(a!=0)
+            System.out.println(b/a);
+            else
+            System.out.println("cant divide");
+        }
+        
+
+    }
     
 }

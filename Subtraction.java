65e01359a7a396926f0138d5d02b0db81aee51e3
Neha Agrahari <neha16agrahari@gmail.com>
2025-01-17 14:50:46 +0530

    update

--- Subtraction.java
+++ Subtraction.java
@@ -9,23 +9,3 @@ public class Subtraction {
     }
     
 }
-import java.util.logging.Logger;
-
-public class Subtraction {
-    private static final Logger logger = Logger.getLogger(Subtraction.class.getName());
-
-    public static void main(String[] args) {
-        logger.info("Program started");
-        Scanner sc = new Scanner(System.in);
-        logger.info("Scanner initialized");
-        int a = sc.nextInt();
-        logger.info("First number input: " + a);
-        int b = sc.nextInt();
-        logger.info("Second number input: " + b);
-        int result = a - b;
-        logger.info("Subtraction result: " + result);
-        System.out.println(result);
-        logger.info("Result printed");
-        sc.close();
-        logger.info("Scanner closed");
-    }
